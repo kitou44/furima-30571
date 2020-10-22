06708cf3dcaac95296582e0526dcebf2163c8a6f
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-10-22 15:13:26 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def index
+    @posts = Post.all# 1番目のレコードを@postに代入
+  end
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
 end
