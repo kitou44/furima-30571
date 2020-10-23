ec03c9ed6eb7a78c1d4088efdd2ccd57797bf7ba
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-10-23 17:08:18 +0900

    フリマアプリのDB設計

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-  def index
-    @posts = Post.all# 1番目のレコードを@postに代入
-  end
-  private
+  before_action :basic_auth
 
+  private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
