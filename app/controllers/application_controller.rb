3aa2797f2a10877c0af58dc3586d4d6ee6fa062c
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-02 21:05:41 +0900

    deviseの実装訂正①

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configre_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday do
     nickname              { 'abe' }
     email                 { 'kkk@gmail.com' }
     password              { '00000000' }
