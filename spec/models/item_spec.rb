73bbad1f59be75bec280d756b16c27aae3ddcc8c
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-11 15:36:27 +0900

    app/views/items/new.html.erb　訂正1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -59,7 +59,7 @@ it "価格の範囲が、¥9,999,999以上の場合、登録できない" do
 end
 
 it "価格の範囲が、¥300以下の場合、登録できない" do
-  @items.shopping_cost_id  = '¥299'
+  @items.price  = '¥299'
   @items.valid?
  expect(@items.errors.full_messages).to include("Price must be greater than 300")
 end
