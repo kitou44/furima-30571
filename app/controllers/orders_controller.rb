229386d7204379c78b77f24a1bd14768fe6ca402
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-18 20:28:41 +0900

    app/controllers/orders_controller.rb コメントアウト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
 
-  def index
-    @item = Item.all
-  end
+  #def index
+   # @item = Item.all
+  #end
 
-   def show
-    @order = Order.find(params[:id])
-   end
+   #def show
+    #@order = Order.find(params[:id])
+   #end
 end
