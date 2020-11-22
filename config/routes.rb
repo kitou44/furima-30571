56e9db5e64a4843226693fa14ca2edf63418ee67
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-22 15:35:06 +0900

    config/rotes.rb訂正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :destroy, :edit, :update, :show]
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :users, only: [:edit, :update]
   
   
-  resources :orders, only: [:index]
+  resources :orders, only: [:index, :create]
   get '/orders/:id', to: 'orders#index', as: 'order'
 end
