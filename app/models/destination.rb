b4a08aa802840fdcc699c361dbb58d26a8b2748d
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-23 10:58:45 +0900

    model/destination.rb 訂正

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,3 @@
 class Destination < ApplicationRecord
-  attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
+  attr_accessor :order_history, :prefecture_id, :city, :post_code, :address, :building_name, :phone_number 
 end
