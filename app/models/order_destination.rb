116cb4c36effe3e9490ea45cdbb835e83fccb6c2
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-23 20:26:59 +0900

    order_destination.rb 書き換え

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination 
   include ActiveModel::Model
-  attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
+  attr_accessor :item_id, :user_id, :order_history, :post_code, :prefecture_id, :city, :building_name, :phone_number
 
   def save
     Order.create(item_id: item_id, user_id:user_id)
