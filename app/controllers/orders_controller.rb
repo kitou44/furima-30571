44faf9d76ef70f65e7fbfbac525374a4e8cc6c06
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-23 11:56:11 +0900

    orders_controller.rb訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,12 @@ class OrdersController < ApplicationController
 
   def index
     # フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-    @order = Order.new
+    @order = Destination.new
 
   end
 
   def create
      # フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-    @order = Order.new(order_parames)
+    @order = Destination.new(order_parames)
   end
 end
