1fb257c3a9905feff329d4707d465de4ced167bc
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-23 19:15:48 +0900

    model/order/desitination.rb 書き換え

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,7 +1,7 @@
-class OrderDestination < ApplicationRecord
+class OrderDestination 
   def save
-    Oerder.create(item: item, user:user)
-    Address.create(order_history: order_history, post_code: post_code, prefecture_id: prefecture_id, city: city, building_name: building_name, user_id: user_id, phone_number :phone_number)
-    
+    Order.create(item_id: item_id, user_id:user_id)
+    Address.create(order_history: order_history, post_code: post_code, prefecture_id: prefecture_id, 
+    city: city,building_name: building_name, user_id: user_id, phone_number :phone_number)
   end
 end
