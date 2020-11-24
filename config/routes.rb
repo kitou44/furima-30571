c19e24c49bf3ec235158aae29b46448632bcb757
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-24 10:18:33 +0900

    config/routes.rb訂正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items
   resources :items do
-    resources :orders, only: :create
+  resources :orders, only: :create
   end
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
