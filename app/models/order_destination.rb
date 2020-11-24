d74100b908eb0b47f23a6b2c668c7c95b9422f64
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-24 15:01:15 +0900

    order_destination.rb　書き換え

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,7 +1,14 @@
 class OrderDestination 
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :order, :post_code, :prefecture_id, :city, :building_name, :phone_number
-  
+
+  with_options presence: true do
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :address, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Include hyphen(-)"}
+    validates :building_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
 
   def save
     Order.create(item_id: item_id, user_id:user_id)
