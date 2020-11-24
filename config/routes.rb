2f849e4f2e39dfb3dcf485f6d11ff42ccfc699b2
kitou44 <71999060+kitou44@users.noreply.github.com>
2020-11-24 11:35:48 +0900

    order/index.html.erb書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'cards/new'
   devise_for :users
   root to: "items#index"
-  resources :items
   resources :items do
-  resources :orders, only: :create
+    resources :orders, only: [:index, :create]
   end
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
-  resources :cards, only: [:new, :create]
-  resources :orders, only: [:index]
-  get '/orders/:id', to: 'orders#index', as: 'order'
 end
